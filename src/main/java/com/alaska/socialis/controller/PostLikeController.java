275a94cdc27aa0e5887390830fb2c600b29305ba
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-10-18 23:24:37 +0000

    update the response for the togglePostLike method

--- src/main/java/com/alaska/socialis/controller/PostLikeController.java
+++ src/main/java/com/alaska/socialis/controller/PostLikeController.java
@@ -1,6 +1,12 @@
 package com.alaska.socialis.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +22,20 @@ public class PostLikeController {
     @Autowired
     private PostLikeService postLikeService;
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @GetMapping("/{userId}/{postId}/like")
-    public PostDto togglePostLike(@PathVariable("userId") Long userId, @PathVariable("postId") Long postId) {
+    public ResponseEntity<Map<String, Object>> togglePostLike(@PathVariable("userId") Long userId,
+            @PathVariable("postId") Long postId) {
         PostDto postDto = this.postLikeService.togglePostLike(userId, postId);
 
-        return postDto;
+        this.messagingTemplate.convertAndSend("/feed/post/update", postDto);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.OK);
+        response.put("message", "Post like updated");
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 }
