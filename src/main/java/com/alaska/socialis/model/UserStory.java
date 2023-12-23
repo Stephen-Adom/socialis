163e3f9ea2b7336002c544c28d1ebd40625bf3a6
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-12-23 20:05:01 +0000

    create model for user story

---
+++ src/main/java/com/alaska/socialis/model/UserStory.java
@@ -0,0 +1,38 @@
+package com.alaska.socialis.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class UserStory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "media_url", nullable = false)
+    private String mediaUrl;
+
+    @Column(name = "media_caption", nullable = true)
+    private String mediaCaption;
+
+    @Column(name = "expired_at", nullable = false)
+    private Date expiredAt;
+
+    @Column(name = "uploaded_at", nullable = false)
+    private Date uploadedAt;
+}
