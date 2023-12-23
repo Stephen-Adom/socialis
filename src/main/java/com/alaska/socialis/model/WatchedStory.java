d6c7974127712f2890ed9b81272c56fd0f5e7313
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-12-23 20:13:19 +0000

    create model for watched story

---
+++ src/main/java/com/alaska/socialis/model/WatchedStory.java
@@ -0,0 +1,32 @@
+package com.alaska.socialis.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class WatchedStory {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "story_id", referencedColumnName = "id")
+    private UserStory story;
+
+    private Date watchedAt;
+}
