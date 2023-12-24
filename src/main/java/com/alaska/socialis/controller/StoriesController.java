e959060da1d63724c9a524002eb1a24139bb48dd
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-12-24 15:56:42 +0000

    update request param in stories controller

--- src/main/java/com/alaska/socialis/controller/StoriesController.java
+++ src/main/java/com/alaska/socialis/controller/StoriesController.java
@@ -3,6 +3,8 @@ package com.alaska.socialis.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.alaska.socialis.exceptions.EntityNotFoundException;
+import com.alaska.socialis.model.dto.SuccessMessage;
 import com.alaska.socialis.services.VideoService;
 
 @RestController
@@ -21,15 +24,17 @@ public class StoriesController {
     private VideoService videoservice;
 
     @PostMapping(value = "/{userId}/upload", headers = "Content-Type=multipart/form-data")
-    public String postStories(@RequestParam(required = true, value = "video") MultipartFile file,
+    public ResponseEntity<SuccessMessage> postStories(
+            @RequestParam(required = true, value = "storyMedia") MultipartFile file,
             @RequestParam(required = false, value = "caption") String caption,
             @PathVariable("userId") Long userId)
             throws IOException, EntityNotFoundException {
 
         this.videoservice.uploadStory(file, caption, userId);
 
-        System.out.println(" ========================= operation done ===========================");
+        SuccessMessage message = SuccessMessage.builder().message("User story uploaded successfully")
+                .status(HttpStatus.OK).build();
 
-        return "";
+        return new ResponseEntity<SuccessMessage>(message, HttpStatus.OK);
     }
 }
